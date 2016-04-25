49adee4668845bc456089ab95396c6d268d20c74
Fernanda Lowe and LaTaevia Berry <lataevia@>
2016-04-25 10:21:32 -0700

    add completed rectangle project

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -15,8 +15,20 @@ public class RectangleTest {
   }
 
   @Test
-  public void getWidth_getsRectangleWidth_2() {
+  public void getWidth_getsRectangleWidth_4() {
     Rectangle testRectangle = new Rectangle(2, 4);
     assertEquals(4, testRectangle.getWidth());
+  }
+
+  @Test
+  public void isSquare_whenNotASquare_false() {
+    Rectangle testRectangle = new Rectangle(2, 4);
+    assertEquals(false, testRectangle.isSquare());
+  }
+
+  @Test
+  public void isSquare_allSidesEqual_true() {
+    Rectangle testRectangle = new Rectangle(2, 2);
+    assertEquals(true, testRectangle.isSquare());
   }
 }
